f92581940617a2899dca3519bacc2431c976b957
Matt Young <dev@mttyng.com>
2023-06-18 17:49:29 -0700

    testing styles

--- src/components/MindMap/MindMap.tsx
+++ src/components/MindMap/MindMap.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Map, Node as NodeModel } from '../../models'
 import { Node } from '../Node'
+import { useMindMap } from '../../hooks'
 
 export interface IMindMap {
   data: Record<string, string>
@@ -9,17 +10,47 @@ export interface IMindMap {
 // TODO remove this hard-coded map/data
 const map = new Map()
 const n1 = new NodeModel({ topic: 'triangle' })
-const n2 = new NodeModel({ topic: 'kimura' })
-map.root?.setChildren([n1, n2])
+map.root?.setChildren([n1])
 map.root?.topic = 'closed guard'
 
 const MindMap: React.FC<IMindMap> = ({ data }) => {
+  const { getPosition } = useMindMap()
+  const [rootCoord, setRootCoord] = React.useState()
+  const [nCoord, setNCoord] = React.useState()
+
+  const rootRef = React.useRef(null)
+  const nRef = React.useRef(null)
+
+  React.useEffect(() => {
+    setRootCoord(getPosition(rootRef))
+    setNCoord(getPosition(nRef))
+  }, [])
+
+  console.log({ rootCoord, nCoord })
   return (
     <div>
+      {rootCoord?.centerH && nCoord?.centerH ? (
+        <svg style={{ position: 'absolute' }} width='1039' height='500'>
+          <defs>
+            <marker id='head' orient='auto' markerWidth='2' markerHeight='4' refX='0.1' refY='2'>
+              <path d='M0,0 V4 L2,2 Z' fill='#555' />
+            </marker>
+          </defs>
+          <path
+            stroke='#555'
+            strokeWidth='2'
+            marker-end='url(#head)'
+            fill='blue'
+            d={`M ${rootCoord.centerH} ${rootCoord.centerV} C ${rootCoord.centerH} ${rootCoord.centerV}, ${nCoord.centerH} ${nCoord.centerV}, ${nCoord.centerH} ${nCoord.centerV}`}
+          ></path>
+        </svg>
+      ) : null}
       <div>MindMap: inner</div>
-      <Node node={map.root} />
+      {/* root */}
+      <Node rref={rootRef} node={map.root} />
+
       {map.root?.children.map((n, i) => (
-        <Node key={i} node={n} />
+        <Node key={i} node={n} rref={nRef} />
       ))}
     </div>
   )
--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { INode } from '../models/node'
 
 const nodeStyle =
-  'inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
+  'inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 relative'
 
-export const Node: React.FC<{ node: INode }> = ({ node }) => {
-  return <div className={nodeStyle}>{node.topic}</div>
+export const Node: React.FC<{ node: INode; rref: any }> = ({ node, rref }) => {
+  return (
+    <div className={nodeStyle} ref={rref}>
+      {node.topic}
+    </div>
+  )
 }
