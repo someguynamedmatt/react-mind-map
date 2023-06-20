49d6923cf78fa516517ad8fb72d3d61caf8a5d4c
Matt Young <dev@mttyng.com>
2023-06-20 15:49:20 -0700

    wip: trying to get click-for-addition of nodes going

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { INode } from '../models/node'
+import { INode, Node as NodeModel } from '../models/node'
 import { useMindMap } from '../hooks'
 import { SvgPath } from './SvgPath'
 
@@ -13,31 +13,49 @@ export const Node: React.FC<{ node: INode }> = React.forwardRef(
     const [thisPosition, setThisPosition] = React.useState(getPosition(ref?.current))
     const [positions, setPositions] = React.useState(getPosition(ref?.current))
 
+    // TODO not sure if this is necessary
     if (childRefs.current.length !== node.children?.length) {
       childRefs.current = Array(node.children?.length)
         .fill()
         .map((_, i) => childRefs.current[i]?.current || React.createRef())
     }
 
-    React.useEffect(() => {
+    const updatePositions = () => {
       setThisPosition(getPosition(ref?.current))
-      setPositions(node.children?.map((n, i) => getPosition(childRefs.current[i].current)))
+      setPositions(node.children?.map((n, i) => getPosition(childRefs?.current[i]?.current)))
+    }
+
+    React.useEffect(() => {
+      updatePositions
     }, [])
 
     React.useEffect(() => {
       const resize = () => {
-        setThisPosition(getPosition(ref?.current))
-        setPositions(node.children?.map((n, i) => getPosition(childRefs.current[i].current)))
+        updatePositions()
       }
       window.addEventListener('resize', resize)
 
       return () => window.removeEventListener('resize', resize)
     }, [])
 
+    React.useEffect(() => {
+      console.log('effect')
+      updatePositions()
+    }, [node.children, childRefs.current.length])
+
+    const onClick = () => {
+      const newNode = new NodeModel({ topic: 'cross-collar', parentId: node.id })
+      node.setChildren([...node.children, newNode])
+      childRefs.current = [...childRefs.current, newNode]
+      console.log(`${node.topic}: set new child`, node.children)
+      console.log(node.children?.length)
+      console.log(childRefs.current)
+    }
+
     return (
       <div className='flex'>
         <div className='z-20 w-[200px]'>
-          <div className={nodeStyle} ref={ref}>
+          <div onClick={onClick} className={nodeStyle} ref={ref}>
             {node.topic}
           </div>
         </div>
